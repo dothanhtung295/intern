86aa8b215f5eca2b09a7b02a3357090c2b28a440
dothanhtung295 <bakura.snow@gmail.com>
2022-06-27 23:44:08 +0700

    my code

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Route;
+Route::get('/', "mycontroller@index");
+Route::get('/add-product','mycontroller@viewAddProduct');
+Route::post('/addProduct','mycontroller@postAddProduct');
+Route::get('/detail-product/{id}','mycontroller@detailProduct');
+Route::get('/edit-product/{id}','mycontroller@editProduct');
+Route::get('/delete-product/{id}','mycontroller@deleteProduct');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
